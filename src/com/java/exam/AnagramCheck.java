40177f37511cab2e532ccd4b5d9cbad9f824d4aa
sujithgoud2210 <sujith200322@gmail.com>
2026-05-11 22:34:00 +0530

    commit

--- src/com/java/exam/AnagramCheck.java
+++ src/com/java/exam/AnagramCheck.java
@@ -1,4 +1,37 @@
 package com.java.exam;
 
+import java.util.Scanner;
+
 public class AnagramCheck {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String str1 = sc.nextLine().replace(" ", "").toLowerCase();
+        String str2 = sc.nextLine().replace(" ", "").toLowerCase();
+
+        int[] freq = new int[26];
+
+        for (int i = 0; i < str1.length(); i++) {
+            freq[str1.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < str2.length(); i++) {
+            freq[str2.charAt(i) - 'a']--;
+        }
+
+        boolean anagram = true;
+
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] != 0) {
+                anagram = false;
+                break;
+            }
+        }
+
+        if (anagram)
+            System.out.println("Anagrams");
+        else
+            System.out.println("Not Anagrams");
+    }
 }
