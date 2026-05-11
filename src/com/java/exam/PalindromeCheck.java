9b6d1d4a752231fda9ebcac7d94a17110bd81404
sujithgoud2210 <sujith200322@gmail.com>
2026-05-11 22:34:28 +0530

    commit

--- src/com/java/exam/PalindromeCheck.java
+++ src/com/java/exam/PalindromeCheck.java
@@ -1,4 +1,35 @@
 package com.java.exam;
 
+import java.util.Scanner;
+
 public class PalindromeCheck {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        str = str.replace(" ", "").toLowerCase();
+
+        int left = 0;
+        int right = str.length() - 1;
+
+        boolean palindrome = true;
+
+        while (left < right) {
+
+            if (str.charAt(left) != str.charAt(right)) {
+                palindrome = false;
+                break;
+            }
+
+            left++;
+            right--;
+        }
+
+        if (palindrome)
+            System.out.println("It is a palindrome!");
+        else
+            System.out.println("Not a palindrome!");
+    }
 }
