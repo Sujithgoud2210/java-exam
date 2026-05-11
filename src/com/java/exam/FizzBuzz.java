f6c51c71133e1602623f9c19ecca3a66c9c646fd
sujithgoud2210 <sujith200322@gmail.com>
2026-05-11 22:35:10 +0530

    commit

--- src/com/java/exam/FizzBuzz.java
+++ src/com/java/exam/FizzBuzz.java
@@ -1,4 +1,22 @@
 package com.java.exam;
 
 public class FizzBuzz {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 50; i++) {
+
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print("FizzBuzz ");
+            }
+            else if (i % 3 == 0) {
+                System.out.print("Fizz ");
+            }
+            else if (i % 5 == 0) {
+                System.out.print("Buzz ");
+            }
+            else {
+                System.out.print(i + " ");
+            }
+        }
+    }
 }
